ef39a0ae6d6320126e37da1ac1aaefbb2e7c6588
dev17 <test@test.com>
2019-01-28 23:58:53 +0300

    Add current status comment in source code

--- src/main/java/com/applabsimple/AppLabSimple/AppLabSimpleApplication.java
+++ src/main/java/com/applabsimple/AppLabSimple/AppLabSimpleApplication.java
@@ -3,6 +3,14 @@ package com.applabsimple.AppLabSimple;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Current status:
+ * Application put into containers:
+ * db - container with PostgreSQL
+ * adminer - manage database in web
+ * simple-app - container with application (in progress)
+ */
+
 @SpringBootApplication
 public class AppLabSimpleApplication {
 
